f8e7b6a8119be2e01ec70c09df8d7d1984bee3a8
Ben Sandler <ben1sandler@gmail.com>
2017-09-24 23:36:00 -0400

    add some more utils, make meetingAt thread safe

--- race/utils.go
+++ race/utils.go
@@ -2,6 +2,18 @@ package race
 
 import "sync"
 
+type lockerString struct {
+	sync.Mutex
+	s string
+}
+
+// put(k,v) maps k to v in the map
+func (l *lockerString) set(k string) {
+	l.Lock()
+	l.s = k
+	l.Unlock()
+}
+
 type concurrentMap struct {
 	sync.RWMutex
 	m map[string]string
@@ -20,4 +32,27 @@ func (c *concurrentMap) get(k string) (string, bool) {
 	v, ok := c.m[k]
 	c.RUnlock()
 	return v, ok
+}
+
+// getPath uses a mapping from nodes to other nodes to compute a path from start
+func getPath(start string, pathMap *concurrentMap) []string {
+	currentNode := start
+	path := make([]string, 0)
+	more := true
+
+	for more && currentNode != "" {
+		path = append(path, currentNode)
+
+		currentNode, more = pathMap.get(currentNode)
+	}
+
+	return path
+}
+
+// reverse a string slice in place
+func reverse(a []string) {
+	for i := len(a)/2 - 1; i >= 0; i-- {
+		opp := len(a) - 1 - i
+		a[i], a[opp] = a[opp], a[i]
+	}
 }
